1d8b1003c2a800f7758e1386fa28e8084159d983
erick <a22006182@alunos.ulht.pt>
2023-10-13 03:51:49 +0100

    Stations country page done,, building map of stations slected, user location functionality added

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('stations/', views.list_stations_view, name='list_stations'),
     path('get-nearest-station/', views.nearest_station, name='nearest_station'),
     path('stations-by-country/<str:country>/', views.stations_by_country_view, name='stations_by_country'),
+    path('stations/<int:station_id>/', views.station_detail, name='station_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from core.models import EVChargingLocation
 from django.http import JsonResponse
 from geopy.distance import geodesic
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
@@ -18,15 +19,21 @@ def map_view(request):
 
 
 def list_stations_view(request):
-    stations = list(EVChargingLocation.objects.values('location_name', 'city_town', 'country')[:1200])
-    context = {'stations': stations}
+    stations = list(EVChargingLocation.objects.values('location_name', 'city_town', 'country'))
+    context = {'stations': stations, 'stations_count': len(stations)}
     return render(request, 'stations.html', context)
 
 
 def stations_by_country_view(request, country):
-    stations = list(EVChargingLocation.objects.filter(country=country)[:1200])
+    stations = list(EVChargingLocation.objects.filter(country=country))
     context = {'stations': stations, 'country': country, 'stations_count': len(stations)}
     return render(request, 'stations_by_country.html', context)
+
+
+def station_detail(request, station_id):
+    station = get_object_or_404(EVChargingLocation, pk=station_id)
+    context = {'station': station}
+    return render(request, 'station_detail.html', context)
 
 
 def nearest_station(request):
@@ -34,8 +41,10 @@ def nearest_station(request):
     longitude = request.GET.get('longitude')
     user_location = latitude, longitude
     station_distances = {}
+    station_name = ''
 
     for station in EVChargingLocation.objects.all()[:1200]:
+        station_name = station.location_name
         station_location = station.latitude, station.longitude
         distance = geodesic(user_location, station_location).km
         station_distances[distance] = station_location
@@ -44,6 +53,7 @@ def nearest_station(request):
     station_coords = station_distances[min_distance]
 
     return JsonResponse({
+        'name': station_name,
         'coordinates': station_coords,
         'distance': min_distance
     })
